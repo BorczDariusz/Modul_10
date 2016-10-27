37534a2c1eaeb0682acf3fff1a97a25f373ad21b
Dariusz Borcz <borcz.dariusz@gmail.com>
2016-10-27 14:11:40 +0200

    dodatkowa gwarancja

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
-function MobilePhones(model, price, color) {
+function MobilePhones(model, price, color, warranty) {
 	this.model = model || 'Ask for model';
 	this.price = price || 'Ask for price';
 	this.color = color || 'Ask for color';
+	this.warranty = this.price * 0.1 || 'Ask for warranty'
 }
+
 MobilePhones.prototype.printInfo = function() {
-	console.log('This mobile phone\'s model is ' + this.model + ', color is ' + this.color + ', price is ' + this.price + '.');
+	console.log('This mobile phone\'s model is ' + this.model + ', color is ' + this.color + ', price is ' + this.price + '. Extended warranty price: ' + this.warranty + '(10%).');
 }
 
 var samsungGalaxyS6 = new MobilePhones('Samsung Galaxy', 1300, 'black/silver/white');
